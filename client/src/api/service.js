df95c0b30c60d686d67c61aac59f66293d5d8506
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-10-07 18:49:57 +0200

    remove reference of localhost/baseurl

--- client/src/api/service.js
+++ client/src/api/service.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const service = axios.create({
   // make sure you use PORT = 5005 (the port where our server is running)
-  baseURL: "http://localhost:5005/api",
   withCredentials: true // => you might need this when having the users in the app
 });
 
@@ -12,20 +11,20 @@ const errorHandler = (err) => {
 
 const handleUpload = (file) => {
   return service
-    .post("/audiofiles/upload", file)
+    .post("/api/audiofiles/upload", file)
     .then((res) => res.data)
     .catch(errorHandler);
 };
 
 const saveNewAudioFile = (newAudioFile) => {
   return service
-    .post("/audiofiles/tones/add", newAudioFile)
+    .post("/api/audiofiles/tones/add", newAudioFile)
     .then((res) => res.data)
     .catch(errorHandler);
 };
 
 const deleteAudioFile = (audioFileId) => {
-    return service.delete(`/audiofiles/${audioFileId}`)
+    return service.delete(`/api/audiofiles/${audioFileId}`)
     .then((res) => res.data)
     .catch(errorHandler);
 }
